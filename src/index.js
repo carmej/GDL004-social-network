29b4a473d30e6db8dc3284e65b13a17b43b6afe1
carmej <carmen.mejia.pinedo@gmail.com>
2020-01-15 12:51:01 -0600

    inicio single page

--- src/index.js
+++ src/index.js
@@ -3,12 +3,3 @@ import { example } from './example.js';
 example();
 
 //var provider = new firebase.auth.GoogleAuthProvider();
-
-function formulario () {
-    var padre = document.getElementById("padre");
-    var input = document.createElement("INPUT");
-    input.type = 'text';
-    padre.appendChild(input);
-}
-document.getElementById('botonregistrarse').addEventListener('click', formulario);
- 
