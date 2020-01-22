2cc94f3d4dd41de27576786fb088858dd7621450
unknown <eugynias>
2020-01-22 09:18:42 -0500

    MitadFormulario

--- src/views/crearcuenta.js
+++ src/views/crearcuenta.js
@@ -1,23 +1,39 @@
 export default ()=> {
-    const viewcrearcuenta=
-    //`<div class="login-container">
-      // <main class="register">
-         //<form class="formulario">
-           //<div>
-            // <p class="titulo/registro">Crea tu cuenta aqui</>
-           //</div>
-           //<div class="input">
-            //<input  id="name" required placeholder="Nombre de Perfil">
-            //<input  id="reg-correo" placeholder="Correo electronico" required>
-            //<input  id="reg-pss" placeholder="Contrasena" required>
-           //</div>
-      //</div> `
-      const formulario=document.getElementById("container")
-      let newInput=document.createElement("input");
-      formulario.appendChild(newInput);
+  //Crear div dentro de mi Container
+  const crearCuenta = document.getElementById("container");
+  const divElement=document.createElement("div");
+  container.appendChild(divElement);
+  divElement.setAttribute("id", "divElement");
 
-    //const divElem = document.createElement('div')
-    //divElem.innerHTML = viewcrearcuenta;
+  //Crear form principal
+  const formCrearCuenta = document.createElement("form")
+  divElement.appendChild(formCrearCuenta);
 
-    return formulario;
-}
+  //Input nombre
+  const inputTextNom=document.createElement("input")
+  inputTextNom.setAttribute("type", "text");
+  inputTextNom.setAttribute("placeholder", "Escribe tu nombre");
+
+  //Input Apellido
+  const inputTextApe=document.createElement("input")
+  inputTextApe.setAttribute("type", "text");
+  inputTextApe.setAttribute("placeholder", "Escribe tu Apellido");
+
+  //Input Correo
+  const inputTextCorreo=document.createElement("input")
+  inputTextCorreo.setAttribute("type", "text");
+  inputTextCorreo.setAttribute("placeholder", "Escribe tu Correo");
+
+  //Input Boton Aceptar....este debe hacer un evento a la funcion de SignU
+  const inputAceptar=document.createElement("input")
+  inputAceptar.setAttribute("type", "submit");
+  inputAceptar.setAttribute("value", "submit");
+
+
+  formCrearCuenta.appendChild(inputTextNom);
+  formCrearCuenta.appendChild(inputTextApe);
+  formCrearCuenta.appendChild(inputTextCorreo);
+  formCrearCuenta.appendChild(inputAceptar);
+
+  return crearCuenta;
+};
