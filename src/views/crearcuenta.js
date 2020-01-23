5bf0e12416aa4795819a3b2ff803a7cca27c8b42
vivianakgp <vivianakgp@gmail.com>
2020-01-23 01:17:41 -0600

    estructura y deseño de las vistas inicio y entrar

--- src/views/crearcuenta.js
+++ src/views/crearcuenta.js
@@ -1,5 +1,49 @@
-export default ()=> {
-    const viewcrearcuenta= `<div class="login-container">    
+ export default ()=> {
+    export default ()=> {
+        const viewcrearcuenta=
+        //Crear div dentro de mi Container
+       // const crearCuenta = document.getElementById("container");//
+        const divElement=document.createElement("div");
+        //container.appendChild(divElement);//
+        divElement.setAttribute("id", "_container");
+        //Crear form principal
+        //const formCrearCuenta = document.createElement("div")
+        //divElement.appendChild(formCrearCuenta);
+        //Input nombre
+        const inputTextNom=document.createElement("input");
+        inputTextNom.setAttribute("type", "text");
+        inputTextNom.setAttribute("placeholder", " Nombre");
+        inputTextNom.setAttribute("class", "item-login");
+
+        //Input Email
+        const inputEmail=document.createElement("input")
+        inputEmail.setAttribute("type", "text");
+        inputEmail.setAttribute("placeholder", "tu Email");
+        inputEmail.setAttribute("class", "item-login");
+        //Input Password
+        const inputPass=document.createElement("input")
+        inputPass.setAttribute("type", "text");
+        inputPass.setAttribute("placeholder", "Password");
+         //Input confirmar Password
+         const inputPass_confirm=document.createElement("input")
+         inputPass_confirm.setAttribute("type", "text");
+         inputPass_confirm.setAttribute("placeholder", " Confirmar Password");
+        //Input Boton Aceptar....este debe hacer un evento a la funcion de SignUp
+        const inputAceptar=document.createElement("input")
+        inputAceptar.setAttribute("type", "submit");
+        inputAceptar.setAttribute("value", "submit");
+        formCrearCuenta.appendChild(inputTextNom);
+        formCrearCuenta.appendChild(inputEmail);
+        formCrearCuenta.appendChild(inputPass);
+        formCrearCuenta.appendChild(inputPass_confirm);
+        formCrearCuenta.appendChild(inputAceptar);
+        return crearCuenta;
+
+    
+}
+
+
+     /*const viewcrearcuenta= `<div class="login-container">    
     <div class="brand-login">     
     <img src="images/homeschooling.png" class="logo"></div>     
     <div class="form-login">       
@@ -8,15 +52,8 @@ export default ()=> {
     <input type="submit" value="Ingresar" class="form-login--submit"> 
     </div>  
     </div>   
-    <div id="iconos">     
-    <a id="google"><img src="images/googleCircle.png" class="pequeña"></a>      
-    <a id="facebook"><img src="images/iconFacebook.png" class="pequeña"></a></div>    
-    <div id="padre">             
-    </div>
-    <p>No tienes cuenta aún?</p>  <p> <input class="button" type="button" value="REGISTRARSE" name="Entrar" id="botonregistrarse"></p>`
+   `
 
-    const divElem = document.createElement('div')
+   const divElem = document.createElement('div')
     divElem.innerHTML = viewcrearcuenta;
-
-    return divElem;
-}
+    retur divElement;*/
--- src/views/home.js
+++ src/views/home.js
@@ -1,5 +1,26 @@
 export default ()=> {
-    const viewhome= `<div class=contenedor-inicio>
+    const viewhome= `
+    -<header>
+   <div class="contenedor">
+      <img class="logo" src="images/hommlogo.png">
+      <h1>LIBRES PARA APRENDER</h1>  
+            <nav class="menu">
+          <ul>
+             <li>
+             <a href="#/">INICIO</a>
+             </li>
+             <li>
+                <a href="#/entrar">ENTRAR</a>
+             </li>
+             <li>
+                <a href="#/crearcuenta">CREAR CUENTA</a>
+             </li>
+           </ul>
+          </nav>
+             
+        </div>
+  </header>
+    <div class=contenedor-inicio>
     <div class="items"> <img src="images/boy-unschooling.jpeg" class="logo-inicio"></div>
     <div class="items"><h2>EDUlarity</h2></div> 
     <div class="items"><p>Bienvenidos A Nuestro Espacio Social De Aprendizaje Sobre Alternativas A La Educacion Tradicional,
